9557c14195101fa194fd18ebbe44d51bfd3f8c3c
BrijeshPaghdal <brijeshpaghdal13@gmail.com>
2023-04-05 17:09:00 +0530

    Seller model added

---
+++ EasyGift_API/Models/Seller.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EasyGift_API.Models
+{
+    public class Seller
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int SellerId { get; set; }
+        [Required]
+        [MaxLength(20)]
+        public string SellerName { get; set; }
+        public string SellerLastName { get; set; }
+        [Required]
+        [MaxLength(20)]
+        public string SellerPhoneNo { get; set; }
+        public string SellerPancardNo { get; set; }
+        public string SellerImage { get; set; }
+        public int SellerStatus { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime? UpdateDate { get; set; }
+        [ForeignKey("SellerLogin")]
+        public int SellerLoginId { get; set; }
+
+    }
+}
