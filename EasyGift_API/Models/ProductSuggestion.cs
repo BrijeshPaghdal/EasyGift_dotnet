294cd20445389d335519bc39332c32ef556c6abd
APTTUS\yagrawal <yagrawal@conga.com>
2023-04-16 18:33:56 +0530

    create , update, get

--- EasyGift_API/Models/ProductSuggestion.cs
+++ EasyGift_API/Models/ProductSuggestion.cs
@@ -1,7 +1,7 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace EasyGift_API.Models.Dto.Get
+namespace EasyGift_API.Models
 {
     public class ProductSuggestion
     {
